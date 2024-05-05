c31fb51de550dcd3826f45be8f4e62a9847d197d
mattvchandler <tardarsauce@gmail.com>
2024-05-05 02:08:54 -0800

    cleanup CEC functions a bit

--- cec.hpp
+++ cec.hpp
@@ -22,14 +22,14 @@ public:
     CEC_Input();
     ~CEC_Input();
 
-    void register_up(std::function<void()> f) { callback_directory_[CEC::CEC_USER_CONTROL_CODE_UP] = std::move(f); }
-    void register_down(std::function<void()> f) { callback_directory_[CEC::CEC_USER_CONTROL_CODE_DOWN] = std::move(f); }
-    void register_left(std::function<void()> f) { callback_directory_[CEC::CEC_USER_CONTROL_CODE_LEFT] = std::move(f); }
-    void register_right(std::function<void()> f) { callback_directory_[CEC::CEC_USER_CONTROL_CODE_RIGHT] = std::move(f); }
-    void register_select(std::function<void()> f) { callback_directory_[CEC::CEC_USER_CONTROL_CODE_SELECT] = std::move(f); }
-    void register_EXIT(std::function<void()> f) { callback_directory_[CEC::CEC_USER_CONTROL_CODE_EXIT] = std::move(f); }
-
     void register_by_code(CEC::cec_user_control_code code, std::function<void()> f) { callback_directory_[code] = std::move(f); }
+
+    void register_up(std::function<void()> f) { register_by_code(CEC::CEC_USER_CONTROL_CODE_UP, f); }
+    void register_down(std::function<void()> f) { register_by_code(CEC::CEC_USER_CONTROL_CODE_DOWN, f); }
+    void register_left(std::function<void()> f) { register_by_code(CEC::CEC_USER_CONTROL_CODE_LEFT, f); }
+    void register_right(std::function<void()> f) { register_by_code(CEC::CEC_USER_CONTROL_CODE_RIGHT, f); }
+    void register_select(std::function<void()> f) { register_by_code(CEC::CEC_USER_CONTROL_CODE_SELECT, f); }
+    void register_EXIT(std::function<void()> f) { register_by_code(CEC::CEC_USER_CONTROL_CODE_EXIT, f); }
 
     operator bool() const { return adapter_; }
     operator CEC::ICECAdapter* () { return adapter_; }
